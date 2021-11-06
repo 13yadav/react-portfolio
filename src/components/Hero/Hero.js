177768fa96deb81fc5096ba4fdd0024664436011
Ranjit Yadav <yadavranjit521@gmail.com>
2021-11-06 17:21:33 +0530

    Add Hero & Project Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,21 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Namastey! <br />
+        I'm Ranjit Yadav
+      </SectionTitle>
+      <SectionText>
+        I'm a Software Developer.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://www.linkedin.com/in/13yadav/'}>
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
